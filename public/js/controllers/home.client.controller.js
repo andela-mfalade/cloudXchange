a91ff6f354b95af8650458c50ee7abe9b040f200
andela-mfalade <mayowa.falade@andela.co>
2015-07-28 13:31:36 +0100

    Activate homepage CTA by implementing view all categories, most viewed and top voted features

--- public/js/controllers/home.client.controller.js
+++ public/js/controllers/home.client.controller.js
@@ -15,8 +15,29 @@ angular.module('resourceModule', ['resourceservice'])
       });
     };
 
-    $scope.showTopVoted = function() {
-      var a = _.sortBy($scope.resources, 'title');
+    $scope.sortByStat = function(param) {
+      var sortedArrayOfResources;
+      console.log($rootScope.allResources, 'All resources in the rootScope')
+      switch (param) {
+        case 'all':
+          sortedArrayOfResources = $rootScope.allResources;
+          break;
+        case 'topVoted':
+          sortedArrayOfResources = _.sortBy($rootScope.allResources, 'likes').reverse();
+          break;
+        case 'mostViewed':
+          sortedArrayOfResources = _.sortBy($rootScope.allResources, 'views').reverse();
+          break;
+        default:
+          sortedArrayOfResources = $rootScope.allResources;
+      }
+      $scope.resources = sortedArrayOfResources;
+      console.log($scope.resources, 'current resources dislayed in scope for category:  ', param);
+    };
+
+    $scope.showMostViewed = function() {
+      var sortedArrayOfResources = _.sortBy($scope.resources, 'views');
+      $scope.resources = sortedArrayOfResources;
     };
 
     $scope.showResourcePage = function(resource_id, category) {
